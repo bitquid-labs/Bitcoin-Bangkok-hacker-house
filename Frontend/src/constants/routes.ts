44c9049e08909fadd78815ff9a00cafbc4fc4e4b
bqalex <alex@bqlabs.xyz>
2024-11-11 02:19:42 -0800

    fix: initial frontend commit

---
+++ Frontend/src/constants/routes.ts
@@ -0,0 +1,19 @@
+import {lazy} from 'react';
+
+const DashboardPage = lazy(() => import('pages/DashboardPage'));
+const StrategyManagerPage = lazy(() => import ('pages/StrategyManagerPage'));
+
+enum AppRoutes {
+  Dashboard = '/dashboard',
+  StrategyManger = '/strategymanager',
+}
+
+export const appRoutes = [
+  {key: 'Dashboard', path: AppRoutes.Dashboard, element: DashboardPage},
+  {key: 'StrategyManger', path: AppRoutes.StrategyManger, element: DashboardPage},
+];
+
+export const headerLinks = [
+  {name: 'dashboard', url: AppRoutes.Dashboard},
+  {name: 'strategymanager', url: AppRoutes.StrategyManger},
+];
