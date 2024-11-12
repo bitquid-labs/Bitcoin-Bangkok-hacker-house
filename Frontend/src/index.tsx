b003c5a4044f17abb140ee70b8d0c4a0c477eedc
bqalex <alex@bqlabs.xyz>
2024-11-11 20:32:07 -0800

    fix: upgrade package

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 import "./global.css";
 import { WagmiProvider } from "wagmi";
 import { config } from "lib/config";
@@ -17,13 +17,15 @@ const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
-    <Web3Provider>
+    <WagmiProvider config={config}>
+    {/* <Web3Provider> */}
       <App />
-    </Web3Provider>
+    {/* </Web3Provider> */}
+    </WagmiProvider>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
--- Frontend/src/lib/config.ts
+++ Frontend/src/lib/config.ts
@@ -1,11 +1,20 @@
-import { http, createConfig } from 'wagmi';
+import { http, createConfig } from 'wagmi'
+import { base, mainnet, optimism } from 'wagmi/chains'
+import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
 import { RootStockTest } from '../constants/chains/RootStockTest';
 
+const projectId = '<WALLETCONNECT_PROJECT_ID>'
+
 export const config = createConfig({
-  // connectors,
-  chains: [RootStockTest],
+  chains: [RootStockTest, base],
+  connectors: [
+    injected(),
+    walletConnect({ projectId }),
+    metaMask(),
+    safe(),
+  ],
   transports: {
     [RootStockTest.id]: http(),
+    [base.id]: http(),
   },
-  ssr: true,
-});
+})
